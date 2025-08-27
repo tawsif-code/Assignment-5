be2e5c933e0d9003776cb804e92e6efa3b1a4550
tawsif-code <mtawsif21@gmail.com>
2025-08-27 19:05:42 +0600

    heart part done

---
+++ script.js
@@ -0,0 +1,36 @@
+const transactionData = []
+
+// add heart feature
+
+document.querySelectorAll(".add-heart").forEach(el=>{el.addEventListener("click",function(e){
+    e.preventDefault()
+    const availableheart = parseInt(document.getElementById("available-heart").innerText)
+    const totalnewheart = 1 + availableheart
+    document.getElementById("available-heart").innerText = totalnewheart
+  })
+})
+
+// call feature
+
+const callALert = {
+    1: "Calling National Emergency (999)",
+    2: "Calling Police (999)",
+    3: "Calling Fire Service (999)",
+    4: "Calling Ambulance (1994-999999)",
+    5: "Calling Women & Child Helpline (109)",
+    6: "Calling Anti-Corruption (106)",
+    7: "Calling Electricity Outage (16216)",
+    8: "Calling Brac (16445)",
+    9: "Calling Bangladesh Railway (163)"
+}
+document.querySelectorAll(".call-btn").forEach(el=>{el.addEventListener("click",function(e){
+    e.preventDefault()
+    const availablecoin = parseInt(document.getElementById("available-coin").innerText)
+    if ( availablecoin <= 0 ){
+        alert("There is no coin.")
+        return
+    }
+    const totalnewcoin = availablecoin - 20 
+    document.getElementById("available-coin").innerText = totalnewcoin
+  })
+})
